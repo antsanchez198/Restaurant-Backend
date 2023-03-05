3ae73369099fce68eaaa874fb8f7468fdcd28026
Anthony Sanchez <antsanchez198@gmail.com>
2023-03-04 21:28:52 -0500

    adding more features to home

--- capstone-front/src/Components/NavBar.js
+++ capstone-front/src/Components/NavBar.js
@@ -9,16 +9,12 @@ export default function NavBar (props) {
         <ul className="navBox">
             {/* <li><a className="navItems">Home</a></li> */}
             <Link to = "/home" className="navItems"> Home </Link>
-            <li><a className="navItems">Contact Us</a></li>
-
+            <Link to = "/menu" className="navItems"> Menu </Link>
+            <Link to = "/contact" className="navItems"> Contact Us </Link>
             {
                 !isLogin?  <Link to = "/account" className="navItems"> Account </Link> : 
-
                 <Link to = "/Profile" className="navItems"> Profile </Link>
-
             }
-
-            {/* <li><a className="navCart">My Cart</a></li> */}
             <Link to = "/cart" className="navCart"> My Cart </Link>
         </ul>
     )
--- capstone-front/src/Pages/HomePage.js
+++ capstone-front/src/Pages/HomePage.js
@@ -2,23 +2,66 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Home from "./Home.css";
-
+import fajitas from "../images/fajitas.JPG";
+import burrito from "../images/burrito.JPG";
+import chile from "../images/chile.JPG";
+import pasta from "../images/pasta.JPG";
+import interior1 from "../images/interior1.jpg";
+import interior2 from "../images/interior2.jpg";
+import tacos from "../images/tacos.jpg";
+import tinga from "../images/tinga.JPG";
 
 export default function HomePage() {
 
   return (
     <>
       <header>
-        <h1>Welcome to <strong>Los Mariachis Mexican Restaurant</strong></h1>
-        <button>Order Online</button>
-        <button>See the Menu</button>
+        <div className="left">
+          <h1><strong>Los Mariachis Mexican Restaurant</strong></h1>
+          <div className="button-container">
+            <button className="btn-lg">Order Online</button>
+            <button className="btn-lg">See the Menu</button>
+          </div>
+        </div>
+        <div className="right">
+          <img class="d-block w-100" src="https://lh3.googleusercontent.com/p/AF1QipPoMifNUL5pV1VSF0W1ov-kEKWDvATQFHxhMvz4=s1360-w1360-h1020" />
+        </div>
       </header>
-      
+
       <body>
-          <section>
-            <h2>The Best Authenthic Mexican Food in Brooklyn</h2>
-          </section>
+        <section className="preview">
+          <h2>Serving Authentic Mexican Food in Brooklyn for over 20 Years</h2>
+        </section>
+
+        <section className="bio">
+          <div className="bio-para-container">
+            <h4 className="para-first">Live Mariachis Every Friday from 8pm to 11pm</h4>
+            <img src="https://lh3.googleusercontent.com/p/AF1QipPrNSzc1rMms-6vBs5W3CIS10RGxwaNJ2vP7Fkf=s1360-w1360-h1020"></img>
+          </div>
+          <div className="bio-para-container">
+            <img src={fajitas} alt="fajitas"></img>
+            <h4 className="para-last">Live Mariachis Every Friday from 8pm to 11pm</h4>
+          </div>
+        </section>
+
+        <section className="images-section">
+          <h2>Gallery</h2>
+          <div className="image-container">
+            <img src={tacos}></img>
+            <img src={interior1}></img>
+            <img src={tinga}></img>
+            <img src={interior2}></img>
+            <img src={chile}></img>
+            <img src={burrito}></img>
+          </div>
+        </section>
       </body>
+
+      <footer>
+        <div className="contact-container">
+          contact us
+        </div>
+      </footer>
     </>
   );
 }
